4d8fe91eba2c89fdc533213f52a6caca3abff71c
Paul Kent <paulandrewkent@hotmail.com>
2025-05-20 07:24:19 +0100

    Just added example proto attritubes to commented out version of the supplierview

--- FlowTutorials.Contracts/Areas/Suppliers/SupplierView.cs
+++ FlowTutorials.Contracts/Areas/Suppliers/SupplierView.cs
@@ -1,3 +1,28 @@
-﻿namespace FlowTutorials.Contracts.Areas.Suppliers;
+﻿using ProtoBuf;
+
+namespace FlowTutorials.Contracts.Areas.Suppliers;
 
 public record class SupplierView(int SupplierID, string CompanyName, int ProductCount);
+
+
+
+/*
+ 
+    [ProtoContract]
+    public record class SupplierView
+    {
+        [ProtoMember(1)] public int    SupplierID   { get; }
+        [ProtoMember(2)] public string CompanyName  { get; } = string.Empty;
+        [ProtoMember(3)] public int    ProductCount { get; }
+
+        private SupplierView() { }
+
+        public SupplierView(int supplierID, string companyName, int productCount)
+        {
+            SupplierID   = supplierID;
+            CompanyName  = companyName;
+            ProductCount = productCount;
+        }
+    }
+
+*/
